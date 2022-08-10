3330026869db28ae52bddafc02763f2fda68e2ba
Mosaad <git@themosaad.com>
2022-08-10 23:27:48 +0200

    use verce.images instead of .images to avoid conflic with official remix config

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,5 @@
 // TODO: import Images type
-/** @type {import('@remix-run/dev').AppConfig & {images: { domains: string[]; remotePatterns?: {protocol?: 'http' | 'https';hostname: string;port?: string;pathname?: string;}[]; sizes: number[]; minimumCacheTTL?: number; formats?: Array<'image/avif' | 'image/webp'>; dangerouslyAllowSVG?: boolean; contentSecurityPolicy?: string; }}} */
+/** @type {import('@remix-run/dev').AppConfig & {vercel:{images: { domains: string[]; remotePatterns?: {protocol?: 'http' | 'https';hostname: string;port?: string;pathname?: string;}[]; sizes: number[]; minimumCacheTTL?: number; formats?: Array<'image/avif' | 'image/webp'>; dangerouslyAllowSVG?: boolean; contentSecurityPolicy?: string; }}}} */
 module.exports = {
   serverBuildTarget: "vercel",
   // When running locally in development mode, we use the built in remix
@@ -7,11 +7,13 @@ module.exports = {
   // so we default back to the standard build output.
   server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
-  images: {
-    sizes: [256, 384, 600, 1000],
-    domains: [],
-    minimumCacheTTL: 60,
-    formats: ['image/webp', 'image/avif'],
+  vercel: {
+    images: {
+        sizes: [256, 384, 600, 1000],
+        domains: [],
+        minimumCacheTTL: 60,
+        formats: ['image/webp', 'image/avif'],
+      },
   },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
